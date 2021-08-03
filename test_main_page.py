57d22ee869524123fdca3ba949bdd8700584fda5
sem4ik13 <51079351+sem4ik13@users.noreply.github.com>
2021-08-03 23:43:58 +0500

    add transition on login_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,9 @@
+from final_project.pages.login_page import LoginPage
 from .pages.main_page import MainPage
 
 def test_guest_can_go_to_login_page(browser):
 	link = "http://selenium1py.pythonanywhere.com/"
 	page = MainPage(browser, link) # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-	page.open()					# открываем страницу
-	page.should_be_login_link()	# выполняем метод страницы — переходим на страницу логина
-	
+	page.open()
+	login_page = page.go_to_login_page()
+	login_page.should_be_login_page()
