d8c0225e614d2cfc45fee3bfb67a5771f4452fd4
sem4ik13 <51079351+sem4ik13@users.noreply.github.com>
2021-08-03 23:58:18 +0500

    ill change

--- pages/main_page.py
+++ pages/main_page.py
@@ -9,6 +9,8 @@ class MainPage(BasePage):
 	def go_to_login_page(self):
 		link = self.is_element_present(*MainPageLocators.LOGIN_LINK)
 		link.click()
+		# alert = self.browser.switch_to.alert
+		# alert.accept()
 		# return LoginPage(browser=self.browser, url=self.browser.current_url)
 	
 	def should_be_login_link(self):
